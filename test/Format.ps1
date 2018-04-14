f0941e21677eff6727b0a4083c7e761928262634
WR STUDENT L123 <wrl123u@student.ccc.edu>
2018-04-14 10:26:49 -0500

    adding sql

---
+++ test/Format.ps1
@@ -0,0 +1,19 @@
+﻿$x=Get-Content C:\users\wrl123u\Documents\list.txt
+foreach($i in $x){
+$qoute='"'
+$comma=','
+Write-host "$($i.Substring(0,1))$qoute$($i.substring(1,10))$qoute$comma$qoute$($i.substring(13,03))$qoute$comma$($i.Substring(16,$i.Substring(16).indexOf(',')))"
+
+
+
+}
+
+$y=Import-Csv 'C:\users\wrl123u\Downloads\CarsFinal.csv'
+
+foreach($i in $y){
+$qoute="'"
+$comma=','
+$par='('
+$parc=')'
+Write-Output "$par$qoute$($i.VIN)$qoute$comma$qoute$($i.Location_ID)$qoute$comma$qoute$($i.Make.Trim())$qoute$comma$qoute$($i.Model)$qoute$comma$($i.Rent)$comma$($i.Year)$parc$comma"
+}
